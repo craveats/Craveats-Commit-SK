431e6335884fe387163e495f43e8163240afbdad
M Saidul Karim <karim_mdsaidul@hotmail.com>
2018-11-15 01:48:30 +1100

    Add project files.

---
+++ WebApplication/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = (Session["AreaVisited"] != null ?
+                ("Last visited: " + Session["AreaVisited"].ToString() + " ") : "");
+            Session["AreaVisited"] = "Home/Index";
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = (Session["AreaVisited"]!=null ? 
+                ("Last visited: " + Session["AreaVisited"].ToString()+ " ") : "") + "Your application description page.";
+            Session["AreaVisited"] = "Home/About";
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            
+            ViewBag.Message = (Session["AreaVisited"] != null ?
+                ("Last visited: " + Session["AreaVisited"].ToString() + " ") : "") + "Your contact page.";
+            Session["AreaVisited"] = "Home/Contact";
+            return View();
+        }
+    }
+}
