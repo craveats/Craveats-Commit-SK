c61dd0c64ed3ebf758d6b5eaec028571c00c3494
M Saidul Karim <karim_mdsaidul@hotmail.com>
2018-11-17 00:14:30 +1100

    Check-in following first Azure deployment

--- WebApplication/App_Start/BundleConfig.cs
+++ WebApplication/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace WebApplication
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-readable.css",
                       "~/Content/site.css"));
         }
     }
---
+++ WebApplication/Models/AuthenticatedUser.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication.DAL;
+using Generic.Obfuscation.TripleDES;
+
+namespace WebApplication.Models
+{
+    [Serializable]
+    public class AuthenticatedUserInfo
+    {
+        private AuthenticateUser_Result _seed;
+
+        public AuthenticatedUserInfo(AuthenticateUser_Result authenticateUser_Result)
+        {
+            _seed = authenticateUser_Result; 
+        }
+
+        public string FullName {
+            get {
+                return string.Format("{0}{1}{2}", _seed?.FirstName, " ", _seed?.Surname).Trim();
+            }
+        }
+
+        public string UserId {
+            get {
+                return DataSecurityTripleDES.GetEncryptedText(_seed?.Id);
+            }
+        }
+    }
+}
